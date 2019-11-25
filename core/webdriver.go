6702afa5451b315ef7ee2b26c381d6eaaf2c4f60
xoreo <mattnappo@gmail.com>
2019-11-25 14:11:50 -0500

    core: Isolating the issue.

--- core/webdriver.go
+++ core/webdriver.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/tebeka/selenium"
 	"github.com/tebeka/selenium/chrome"
@@ -80,6 +82,61 @@ func (cwd *ChromeWebDriver) Start() error {
 	if err != nil {
 		return err
 	}
+	err = webDriver.Get("http://play.golang.org/?simple=1")
+	if err != nil {
+		return err
+	}
+
+	elem, err := webDriver.FindElement(selenium.ByCSSSelector, "#code")
+	if err != nil {
+		return err
+	}
+
+	err = elem.Clear()
+	if err != nil {
+		return err
+	}
+
+	// Enter some new code in text box.
+	err = elem.SendKeys(`
+		package main
+		import "fmt"
+		func main() {
+			fmt.Println("Hello WebDriver thing!\n")
+		}
+	`)
+	if err != nil {
+		return err
+	}
+
+	// Click the run button.
+	btn, err := webDriver.FindElement(selenium.ByCSSSelector, "#run")
+	if err != nil {
+		return err
+	}
+	if err := btn.Click(); err != nil {
+		return err
+	}
+
+	// Wait for the program to finish running and get the output.
+	outputDiv, err := webDriver.FindElement(selenium.ByCSSSelector, "#output")
+	if err != nil {
+		return err
+	}
+
+	var output string
+	for {
+		output, err = outputDiv.Text()
+		if err != nil {
+			return err
+		}
+		if output != "Waiting for remote server..." {
+			break
+		}
+		time.Sleep(time.Millisecond * 100)
+	}
+
+	fmt.Printf("%s", strings.Replace(output, "\n\n", "\n", -1))
 
 	// defer webDriver.Quit()
 
