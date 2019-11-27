e03f2a4dbd2701a741f75885c5683a303d0de598
xoreo <mattnappo@gmail.com>
2019-11-27 11:49:13 -0500

    main: Removed example code.

--- scraper/scrape.go
+++ scraper/scrape.go
@@ -11,7 +11,6 @@ import (
 
 // Scrape scrapes stuff using a core.ChromeWebDriver.
 func Scrape(driver *selenium.WebDriver) error {
-
 	err := (*driver).Get("http://play.golang.org/?simple=1")
 	if err != nil {
 		return err
